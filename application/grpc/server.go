cff0c9db836f67ba05511492e808f0eee7d2c830
Wesley Castilho <wesley.castilho8@gmail.com>
2021-02-10 13:34:05 -0300

    gRPC works

--- application/grpc/server.go
+++ application/grpc/server.go
@@ -2,14 +2,25 @@ package grpc
 
 import (
 	"fmt"
+	"github.com/WesleyCastilho/codepix/application/grpc/pb"
+	"github.com/WesleyCastilho/codepix/application/usecase"
+	"github.com/WesleyCastilho/codepix/infrastructure/repository"
 	"github.com/jinzhu/gorm"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 )
 
 func StartGrpcServer(database *gorm.DB, port int){
 	grpcServer:= grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pixRepository := repository.PixKeyRepositoryDb{Db:database}
+	pixUseCase := usecase.PixUseCase{ PixKeyRepository: pixRepository}
+	pixGrpcService := NewPixGrpcService(pixUseCase)
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
+
 
 	address := fmt.Sprintf("0.0.0.0:%d",port)
 	listener, err := net.Listen("tcp", address)
